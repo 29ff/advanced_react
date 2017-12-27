6479bddb49d919667924784f4e3ca9346cd5f375
29ff <nbhung1809@gmail.com>
2017-12-27 17:10:44 +0700

    refactor components

--- lib/components/Timestamp/Timestamp.jsx
+++ lib/components/Timestamp/Timestamp.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import storeProvider from '../storeProvider'
 
-class TimeStamp extends Component {
+class TimeStamp extends PureComponent {
   render() {
     return (
       <div>
